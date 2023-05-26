742b7b1e4b70953f4f8f21ac083de7d880b50fb7
sadmann7 <sadmann898@gmail.com>
2023-05-27 01:11:31 +0600

    set prisma db

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { faker } from "@faker-js/faker"
+import { PrismaClient, type Skater } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.skater.deleteMany()
+
+  for (let i = 0; i < 240; i++) {
+    await prisma.skater.create({
+      data: {
+        name: faker.person.firstName(),
+        age: faker.number.int({ min: 10, max: 60 }),
+        email: faker.internet.email(),
+        stats: faker.number.int({ min: 10, max: 100 }),
+        stance:
+          faker.helpers.shuffle<Skater["stance"]>(["mongo", "goofy"])[0] ??
+          "goofy",
+        deckPrice: faker.number.int({ min: 25, max: 100 }),
+        createdAt: faker.date.past(),
+      },
+    })
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
